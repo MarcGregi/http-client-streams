fbee009b1d9ca6047987fbc58ad8e694a8d07eef
marcgregi <marc@hypedigital.co>
2019-01-13 20:08:54 +0000

    Make a simple request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,21 @@
+// Require the HTTPS Module
 var https = require('https');
 
 console.log('I did it!');
+
+// Make a simple request
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback');
+}
+
+console.log("I'm about to make the request ");
+
+https.request(options, callback).end();
+
+console.log("I've made the request");
