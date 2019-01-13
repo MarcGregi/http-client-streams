31d82dba43af3c67d2ff66300cba32b10096d750
marcgregi <marc@hypedigital.co>
2019-01-13 20:17:40 +0000

    Switching the options object to retrieve data from a different site in byte sized chunks

--- https_example.js
+++ https_example.js
@@ -5,8 +5,8 @@ console.log('I did it!');
 
 // Make a simple request
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
